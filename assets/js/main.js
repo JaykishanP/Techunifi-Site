a3fcd183b8fa7e0c216b3003a6fd3457dd314f52
SahanaLRGowda <160768472+SahanaLRGowda@users.noreply.github.com>
2024-03-01 04:04:32 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,99 +95,6 @@
     }
   });
 
-  /**
-   * Clients Slider
-   */
-  new Swiper('.clients-slider', {
-    speed: 7000,
-    loop: true,
-    autoplay: {
-      delay: 100,
-      disableOnInteraction: false,
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 40
-      },
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 60
-      },
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 80
-      },
-      992: {
-        slidesPerView: 5,
-        spaceBetween: 120
-      }
-    }
-  });
-
-
-  /**
-   * Portfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-      });
-
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        portfolioIsotope.on('arrangeComplete', function() {
-          AOS.refresh();
-        });
-      }, true);
-    }
-
-  });
-
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-
 
   /**
    * Animation on scroll
@@ -215,11 +122,11 @@ var swiper = new Swiper('.bk-slider .swiper', {
   slidesPerView: 3,
   speed: 500,
   centeredSlides: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-    pauseOnMouseEnter: true
-  },
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  //   pauseOnMouseEnter: true
+  // },
   loop: true,
   spaceBetween: 20,
   pagination: {
@@ -233,34 +140,27 @@ var swiper = new Swiper('.bk-slider .swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 1,
+    },
+    1023: {
+      slidesPerView: 1,
+    }
+    ,
+    1024: {
+      slidesPerView: 3,
+    }
+  }
 });
 
 
-
-/* Underline Animation */
-document.addEventListener('DOMContentLoaded', function() {
-  window.addEventListener('scroll', function() {
-    var currentSection = '';
-
-    // Get the id of the section currently in view
-    document.querySelectorAll('.section-container').forEach(function(div) {
-      var rect = div.getBoundingClientRect();
-      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-        currentSection = div.getAttribute('id');
-      }
-    });
-
-    // Remove active class from all h2 tags
-    document.querySelectorAll('h3').forEach(function(h3) {
-      h3.classList.remove('active');
-    });
-
-    // Add active class to the current section's h2
-    document.querySelector('h3[data-target="' + currentSection + '"]').classList.add('active');
-  });
-});
-
-// 
 // Menu
 
 const dropdownBtn = document.querySelectorAll(".dropdown-btn");
@@ -339,10 +239,20 @@ $(document).ready(function() {
 
 $(".nav-end").click (function () {
   $ (".menu").removeClass('menu-expanded');
-  // $ (".menu").toggleClass('show');
 });
 
 });
+
+// mobile li active underline
+
+function toggleUnderline(event) {
+  var allMenuItems = document.querySelectorAll('.menu-bar li .nav-link');
+  allMenuItems.forEach(function(item) {
+    item.classList.remove('underline');
+  });
+  event.target.classList.add('underline');
+}
+
 
 
 // Services
@@ -377,5 +287,11 @@ function openTab(evt, tabName) {
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
+//scroll to top on tab click
+$('.tab button').click(function(event) {
+  event.preventDefault();
 
-/*Vertical tabs - line */
+  $('html,body').animate({scrollTop:0}, 400); 
+});
+
+
