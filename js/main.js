b621945f6d93daf6b148a31805c732f3818c3d2b
SahanaLRGowda <160768472+SahanaLRGowda@users.noreply.github.com>
2024-03-08 05:29:17 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,439 @@
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener);
+  };
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header');
+    let offset = header.offsetHeight;
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 16;
+    }
+
+    let elementPos = select(el).offsetTop;
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    });
+  };
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#nav-menu');
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled');
+      } else {
+        selectHeader.classList.remove('header-scrolled');
+      }
+    };
+    window.addEventListener('load', headerScrolled);
+    onscroll(document, headerScrolled);
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top');
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active');
+      } else {
+        backtotop.classList.remove('active');
+      }
+    };
+    window.addEventListener('load', toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+  }
+
+  $('.back-to-top').click(function(event) {
+    event.preventDefault();
+  
+    $('html,body').animate({scrollTop:0}, 400); 
+  });
+  
+
+
+  /**
+   * Scroll with offset on page load with hash links in the URL
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash);
+      }
+    }
+  });
+
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
+
+})();
+
+
+
+/* Slick Slider*/
+var swiper = new Swiper('.bk-slider .swiper', {
+  slidesPerView: 3,
+  speed: 500,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true
+  },
+  loop: true,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 1,
+    },
+    1023: {
+      slidesPerView: 1,
+    }
+    ,
+    1024: {
+      slidesPerView: 3,
+    }
+  }
+});
+
+// About pagination
+
+function showPage(pageNumber) {
+  var newsSections = document.querySelectorAll('.news-section');
+  var itemsPerPage = 3; // Change this value to adjust the number of items per page
+
+  for (var i = 0; i < newsSections.length; i++) {
+      if (i < pageNumber * itemsPerPage && i >= (pageNumber - 1) * itemsPerPage) {
+          newsSections[i].style.display = 'block';
+      } else {
+          newsSections[i].style.display = 'none';
+      }
+  }
+
+  // Scroll to the top of the news section
+  document.getElementById('tab2').scrollIntoView({ behavior: 'smooth' });
+}
+
+// Function to generate pagination controls
+function setupPagination() {
+  var newsSections = document.querySelectorAll('.news-section');
+  var itemsPerPage = 2; // Change this value to adjust the number of items per page
+  var numPages = Math.ceil(newsSections.length / itemsPerPage);
+
+  var pagination = document.getElementById('news-pagination');
+  pagination.innerHTML = '';
+
+  for (var i = 1; i <= numPages; i++) {
+      var button = document.createElement('button');
+      button.textContent = i;
+      button.addEventListener('click', function() {
+          showPage(parseInt(this.textContent));
+      });
+      pagination.appendChild(button);
+  }
+
+  // Show the first page by default
+  showPage(1);
+}
+
+// Call the setupPagination function when the page loads
+document.addEventListener('DOMContentLoaded', function() {
+    if(document.getElementById('news-pagination')) {
+        setupPagination();
+    }
+});
+
+
+
+/* ==== Submit ticket ===== */
+
+$('option').mousedown(function(e) {
+  e.preventDefault();
+  $(this).prop('selected', !$(this).prop('selected'));
+  return false;
+});
+
+
+/**
+   * Menu
+   */
+// Menu
+
+const dropdownBtn = document.querySelectorAll(".dropdown-btn");
+const dropdown = document.querySelectorAll(".dropdown");
+const hamburgerBtn = document.getElementById("hamburger");
+const navMenu = document.querySelector(".menu");
+const links = document.querySelectorAll(".dropdown a");
+
+function setAriaExpandedFalse() {
+  dropdownBtn.forEach((btn) => btn.setAttribute("aria-expanded", "false"));
+}
+
+function closeDropdownMenu() {
+  dropdown.forEach((drop) => {
+    drop.classList.remove("active");
+    drop.addEventListener("click", (e) => e.stopPropagation());
+  });
+}
+
+function toggleHamburger() {
+  navMenu.classList.toggle("show");
+}
+
+dropdownBtn.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    const dropdownIndex = e.currentTarget.dataset.dropdown;
+    const dropdownElement = document.getElementById(dropdownIndex);
+
+    dropdownElement.classList.toggle("active");
+    dropdown.forEach((drop) => {
+      if (drop.id !== btn.dataset["dropdown"]) {
+        drop.classList.remove("active");
+      }
+    });
+    e.stopPropagation();
+    btn.setAttribute(
+      "aria-expanded",
+      btn.getAttribute("aria-expanded") === "false" ? "true" : "false"
+    );
+  });
+});
+
+// close dropdown menu when the dropdown links are clicked
+links.forEach((link) =>
+  link.addEventListener("click", () => {
+    closeDropdownMenu();
+    setAriaExpandedFalse();
+    toggleHamburger();
+  })
+);
+
+// close dropdown menu when you click on the document body
+document.documentElement.addEventListener("click", () => {
+  closeDropdownMenu();
+  setAriaExpandedFalse();
+});
+
+// close dropdown when the escape key is pressed
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    closeDropdownMenu();
+    setAriaExpandedFalse();
+  }
+});
+
+// toggle hamburger menu
+hamburgerBtn.addEventListener("click", toggleHamburger);
+
+
+// 
+$(document).ready(function() {
+
+ $(".menu-bar > li").click (function () {
+  $ (".menu").addClass('menu-expanded');
+});
+
+$(".nav-end").click (function () {
+  $ (".menu").removeClass('menu-expanded');
+});
+
+});
+
+// mobile li active underline
+
+function toggleUnderline(event) {
+  var allMenuItems = document.querySelectorAll('.menu-bar li .nav-link');
+  allMenuItems.forEach(function(item) {
+    item.classList.remove('underline');
+  });
+  event.target.classList.add('underline');
+}
+
+
+
+// Services
+
+// $(".menu-bar > li").click (function () {
+//   $ (".menu").css('display', 'none');
+// });
+
+// $(".nav-end,").click (function () {
+//   $ (".menu").css('display', 'block');
+// });
+
+
+/* About tabs */
+// Tabs
+
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+// document.getElementById("defaultOpen").click();
+
+document.addEventListener('DOMContentLoaded', function() {
+  var defaultOpenButton = document.getElementById("defaultOpen");
+  if (defaultOpenButton) {
+      defaultOpenButton.click();
+  } else {
+      // console.error("Element with ID 'defaultOpen' not found.");
+  }
+});
+
+
+//scroll to top on tab click
+$('.tab button').click(function(event) {
+  event.preventDefault();
+
+  $('html,body').animate({scrollTop:0}, 400); 
+});
+
+
+/**
+   * Products -left Nav
+*/
+// Products -left Nav
+
+
+
+
+
+//Product - Tabs
+
+function openProd(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("product-tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("prod-tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+// document.getElementById("productOpen").click();
+
+document.addEventListener('DOMContentLoaded', function() {
+  var productOpenButton = document.getElementById("productOpen");
+  if (productOpenButton) {
+    productOpenButton.click();
+  } else {
+      // console.error("Element with ID 'defaultOpen' not found.");
+  }
+});
+
+
+//Prod tab Mobile slide
+// Previous and Next Button functionality
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(function() {
+
+    document.addEventListener("DOMContentLoaded", function() {
+      // Add event listener for the previous button
+      document.querySelector(".prod-prev-button").addEventListener("click", function() {
+        const tabsContainer = document.querySelector(".tabs-container");
+        tabsContainer.scrollLeft -= 200; // Scroll left by 200 pixels
+      });
+    
+      // Add event listener for the next button
+      document.querySelector(".prod-next-button").addEventListener("click", function() {
+        const tabsContainer = document.querySelector(".tabs-container");
+        tabsContainer.scrollLeft += 200; // Scroll right by 200 pixels
+      });
+    });
+
+  }, 500); 
+});
+
+
+
+
+
+
