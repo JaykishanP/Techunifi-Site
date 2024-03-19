28a559279ff2fc57a56ec709e60bc879a5d7bdaf
SahanaLRGowda <160768472+SahanaLRGowda@users.noreply.github.com>
2024-03-20 03:30:03 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -554,3 +554,19 @@ new Swiper('.home-clients-slider', {
 });
 
 
+/* ======== Products Slider ========== */
+const sliderProd = document.querySelector("#js-slider");
+
+new Swiper(sliderProd, {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
