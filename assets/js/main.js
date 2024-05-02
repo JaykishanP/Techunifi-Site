7130de0880fe8c6b40256b48e04b5220a4084d5b
SahanaLRGowda <160768472+SahanaLRGowda@users.noreply.github.com>
2024-05-02 20:18:37 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -983,6 +983,10 @@ $(document).ready(function() {
   });
 
 
+  /* ==== Captcha Implementaion ==== */
+  function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+
 /* ==== Tooltip ==== */
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
